db7762efa9afe742a5edbfb53828cd67ab4da251
HG King <hg@elephantventures.com>
2021-08-26 17:31:12 -0400

    init commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Real Estate Plugin
+
+#### Very quick and dirty POC for a plugin that will pull some information from Zillow and do a quick calculation.
+
+### Setup
+1. Download the code
+1. Go to `chrome://extensions/` in your chrome browser
+1. Click the `Load Unpacked` button in the top left corner of the screen
+1. Select the folder for this repo
+1. Navigate to a Zillow listing of your choice (options listed below)
+1. Go to the plugins list in the top right corner of your browser
+1. Click the Real Estate Plugin button
+1. Click the grey square
+1. See the alert message pop up with some numbers
---
+++ popup.js
@@ -0,0 +1,48 @@
+// Initialize butotn with users's prefered color
+let changeColor = document.getElementById("changeColor");
+
+// When the button is clicked, inject doCalc into current page
+changeColor.addEventListener("click", async () => {
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    function: doCalc,
+  });
+});
+
+// The body of this function will be execuetd as a content script inside the
+// current page
+function doCalc() {
+  const cost_selectors = [
+    "#details-page-container > div > div > div.layout-wrapper > div.layout-container > div.data-column-container > div.summary-container > div > div.ds-home-details-chip > div.ds-summary-row-container > div > div > span > span > span",
+    "#home-details-content > div > div > div.layout-wrapper > div.layout-container > div.data-column-container > div.summary-container > div > div.ds-home-details-chip > div.ds-summary-row-container > div > div > span > span > span"
+  ];
+
+  const monthly_selectors = [
+    "#home-details-content > div > div > div.layout-wrapper > div.layout-container > div.data-column-container > div.summary-container > div > div.ds-home-details-chip > div.ds-mortgage-row > div > span:nth-child(2)",
+    "#details-page-container > div > div > div.layout-wrapper > div.layout-container > div.data-column-container > div.summary-container > div > div.ds-home-details-chip > div.ds-mortgage-row > div > span:nth-child(2)"
+  ];
+  
+  const scrapeElement = (selectors) => {
+    let element = null;
+    let i = 0;
+    while ( element == null && i < selectors.length ) {
+      element = document.querySelector(selectors[i]);
+      i += 1;
+    }
+    return element.innerHTML;
+  }
+
+  let host = window.location.host;
+  let m = host == "www.zillow.com";
+
+  if (m) {
+    let cost = scrapeElement(cost_selectors);
+    let monthly = scrapeElement(monthly_selectors);
+
+    alert("cost: "+ cost + "; monthly" + monthly);
+  } else {
+    alert("Sorry, not on Zillow!");
+  }
+}

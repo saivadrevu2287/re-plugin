d1c73ace8683ed0add94da90694ceceb4bd38f30
HGimself <hgmaxwellking@gmail.com>
2021-09-06 12:54:35 -0400

    rent range up and working like a charm

--- popup.js
+++ popup.js
@@ -4,8 +4,6 @@ const actionButton = document.getElementById("action-button");
 const purchasePriceElement = document.getElementById("purchase-price");
 const estimatePriceElement = document.getElementById("estimate-price");
 
-const offerElement = document.getElementById("offer");
-
 const monthlyTaxesElement = document.getElementById("monthly-taxes");
 const monthlyRentElement = document.getElementById("monthly-rent");
 
@@ -17,15 +15,27 @@ const cashOnCashElement = document.getElementById("cash-on-cash");
 const redfinLinkElement = document.getElementById("redfin-link");
 const realtorLinkElement = document.getElementById("realtor-link");
 
-const sliderElement = document.getElementById("slider");
-const sliderMinElement = document.getElementById("slider-min");
-const sliderMaxElement = document.getElementById("slider-max");
+// for the offer slider and the value it holds
+const offerElement = document.getElementById("offer");
+const offerSliderElement = document.getElementById("offer-slider");
+const offerSliderMinElement = document.getElementById("offer-slider-min");
+const offerSliderMaxElement = document.getElementById("offer-slider-max");
 
-const percentRange = 0.10
-sliderMinElement.innerHTML = `-${100*percentRange}%`;
-sliderMaxElement.innerHTML = `+${100*percentRange}%`;
+// for the rent slider and the value it holds
+const rentElement = document.getElementById("rent");
+const rentSliderElement = document.getElementById("rent-slider");
+const rentSliderMinElement = document.getElementById("rent-slider-min");
+const rentSliderMaxElement = document.getElementById("rent-slider-max");
 
+const offerPercentRange = 0.10
+offerSliderMinElement.innerHTML = `-${100*offerPercentRange}%`;
+offerSliderMaxElement.innerHTML = `+${100*offerPercentRange}%`;
 
+const rentPercentRange = 0.20
+rentSliderMinElement.innerHTML = `-${100*rentPercentRange}%`;
+rentSliderMaxElement.innerHTML = `+${100*rentPercentRange}%`;
+
+// vars for the data that comes over
 let purchasePrice;
 let monthlyTaxes;
 let monthlyRent;
@@ -34,6 +44,10 @@ let estimate;
 let bedsBath;
 let daysOnMarket;
 let cashOnCash;
+
+// vars for the slider values
+let offer;
+let rent;
 
 // When the button is clicked, inject doCalc into current page
 actionButton.addEventListener("click", async () => {
@@ -45,13 +59,20 @@ actionButton.addEventListener("click", async () => {
   }, handleResults);
 });
 
-sliderElement.addEventListener("change", (e) => {
-  cashOnCashElement.innerHTML = doCOC(`${e.target.value}`, monthlyTaxes, monthlyRent);
-  offerElement.innerHTML = e.target.value;
+offerSliderElement.addEventListener("change", (e) => {
+  offer = `$${e.target.value.toLocaleString()}`;
+  cashOnCashElement.innerHTML = doCOC(offer, monthlyTaxes, rent);
+  offerElement.innerHTML = offer;
+});
+
+rentSliderElement.addEventListener("change", (e) => {
+  rent = `$${e.target.value.toLocaleString()}/mo`;
+  cashOnCashElement.innerHTML = doCOC(offer, monthlyTaxes, rent);
+  rentElement.innerHTML = rent;
 });
 
 // turn anything with numbers into just a regular integer
-const toInt = (n) => parseInt(n.split("").filter(a => parseInt(a) >= 0).join(""));
+const toInt = (n) => parseInt(n.split("").filter(a => a.match(/[0-9.]/g)).join(""));
 
 // generate a url that will send us to a google link
 const googleSearchQuery = (s) => `https://www.google.com/search?q=${encodeURIComponent(s)}&btnI`;
@@ -109,9 +130,10 @@ const doCOC = (purchasePrice, monthlyTaxes, monthlyRent) => {
   const monthlyRentNum = toInt(monthlyRent);
 
   if (purchasePriceNum && monthlyTaxesNum && monthlyRentNum) {
-    const coc = calculateCOC(purchasePriceNum, monthlyTaxesNum, monthlyRentNum).toLocaleString() + "%";
+    const coc = calculateCOC(purchasePriceNum, monthlyTaxesNum, monthlyRentNum);
+    const cocString = coc.toLocaleString() + "%";
     setCocClass(coc);
-    return coc;
+    return cocString;
   } else {
     return 'N/A';
   }
@@ -121,7 +143,8 @@ const doCOC = (purchasePrice, monthlyTaxes, monthlyRent) => {
 // it executes in the DOM of the popup
 const handleResults = (r) => {
   const results = r[0].result;
-  // console.log(results);
+
+  // destructure the results
   purchasePrice = results.purchasePrice;
   monthlyTaxes = results.monthlyTaxes;
   monthlyRent = results.monthlyRent;
@@ -130,11 +153,28 @@ const handleResults = (r) => {
   bedsBath = results.bedsBath;
   daysOnMarket = results.daysOnMarket;
 
-  cashOnCash = doCOC(purchasePrice, monthlyTaxes, monthlyRent);
+  // set these for working calculations
+  offer = purchasePrice;
+  rent = monthlyRent;
+
+  // major calculation
+  cashOnCash = doCOC(offer, monthlyTaxes, rent);
+
+  const calculations = {
+    purchasePrice,
+    monthlyTaxes,
+    monthlyRent,
+    address,
+    estimatePrice,
+    bedsBath,
+    daysOnMarket,
+    cashOnCash,
+  }
 
   let redfinSearch = googleSearchQuery(address + " redfin");
   let realtorSearch = googleSearchQuery(address + " realtor");
 
+  // update the ui
   purchasePriceElement.innerHTML = purchasePrice;
   estimatePriceElement.innerHTML = estimatePrice;
   monthlyTaxesElement.innerHTML = monthlyTaxes;
@@ -148,14 +188,33 @@ const handleResults = (r) => {
   redfinLinkElement.href = redfinSearch;
   realtorLinkElement.href = realtorSearch;
 
-  offerElement.innerHTML = toInt(purchasePrice);
-  sliderElement.min = toInt(purchasePrice) * (1 - percentRange);
-  sliderElement.max = toInt(purchasePrice) * (1 + percentRange);
-  sliderElement.value = toInt(purchasePrice);
+  offerElement.innerHTML = offer;
+  offerSliderElement.min = toInt(offer) * (1 - offerPercentRange);
+  offerSliderElement.max = toInt(offer) * (1 + offerPercentRange);
+  offerSliderElement.value = toInt(offer);
+
+  rentElement.innerHTML = rent;
+  rentSliderElement.min = toInt(rent) * (1 - rentPercentRange);
+  rentSliderElement.max = toInt(rent) * (1 + rentPercentRange);
+  rentSliderElement.value = toInt(rent);
 
   actionButton.innerHTML = "Coppied to Clipboard!"
 
-  setCocClass(cashOnCash);
+  const separator = "\n";
+  const toCsv = (obj) => Object.keys(obj).reduce((acc, key) => `${acc}${obj[key]}${separator}`, "");
+  const copy = function (e) {
+      e.preventDefault();
+      const text = toCsv(calculations);
+      if (e.clipboardData) {
+          e.clipboardData.setData('text/plain', text);
+      } else if (window.clipboardData) {
+          window.clipboardData.setData('Text', text);
+      }
+  }
+
+  window.addEventListener('copy', copy);
+  document.execCommand('copy');
+  window.removeEventListener('copy', copy);
 }
 
 // The body of this function will be execuetd as a content script inside the
@@ -192,7 +251,8 @@ const getElements = () => {
   ]
 
   const daysOnMarketSelectors = [
-    "#ds-data-view > ul > li:nth-child(3) > div > div > div.ds-expandable-card-section-default-padding > div.hdp__sc-1f3vlqq-0.jRmwCk > div:nth-child(1) > div.Text-c11n-8-48-0__sc-aiai24-0.fGOvOB"
+    "#ds-data-view > ul > li:nth-child(3) > div > div > div.ds-expandable-card-section-default-padding > div.hdp__sc-1f3vlqq-0.jRmwCk > div:nth-child(1) > div.Text-c11n-8-48-0__sc-aiai24-0.fGOvOB",
+    "#ds-data-view > ul > li:nth-child(3) > div > div > div.ds-expandable-card-section-default-padding > div.sc-ptScb.hfNvvF > div:nth-child(1) > div.Text-c11n-8-48-0__sc-aiai24-0.fGOvOB",
   ]
 
   const scrapeElement = (selectors) => {
@@ -237,21 +297,6 @@ const getElements = () => {
       bedsBath,
       daysOnMarket,
     }
-
-    const copy = function (e) {
-        e.preventDefault();
-        console.log('copy');
-        var text = toCsv(results);
-        if (e.clipboardData) {
-            e.clipboardData.setData('text/plain', text);
-        } else if (window.clipboardData) {
-            window.clipboardData.setData('Text', text);
-        }
-    }
-
-    window.addEventListener('copy', copy);
-    document.execCommand('copy');
-    window.removeEventListener('copy', copy);
 
     return (results);
 
